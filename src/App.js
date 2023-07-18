a0933acff54b05a29a1bd077d0895c3a0c83e27d
songjinhoon <jinhoon0319@gmail.com>
2023-07-18 21:53:11 +0900

    input component add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'react-native';
 import styled, { ThemeProvider } from 'styled-components/native';
 import { theme } from './theme';
+import Input from './component/input';
+import { useState } from 'react';
 
 const Container = styled.View`
   flex: 1;
@@ -12,21 +14,33 @@ const Container = styled.View`
 const Title = styled.Text`
   font-size: 40px;
   font-weight: 600;
-  color: ${({theme}) => theme.main};
+  color: ${({ theme }) => theme.main};
   width: 100%;
   align-items: flex-end;
   padding: 0 20px;
 `;
 
 export default function App() {
+  const [newTask, setNewTask] = useState('');
+  const addTask = () => {
+    alert(newTask);
+    setNewTask('');
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <Container>
-        <Title>TODO List</Title>
         <StatusBar
           barStyle={'light-content'}
           backgroundColor={theme.background}
         ></StatusBar>
+        <Title>TODO List</Title>
+        <Input
+          placeholder={'+ Add a task!'}
+          value={newTask}
+          onChangeText={(text) => setNewTask(text)}
+          onSubmitEditing={addTask}
+        ></Input>
       </Container>
     </ThemeProvider>
   );
---
+++ src/component/input.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+import PropTypes from 'prop-types';
+
+const StyledInput = styled.TextInput.attrs(({ theme }) => ({
+  placeholderTextColor: theme.main,
+}))`
+  width: ${({ width }) => width - 40}px;
+  height: 60px;
+  margin: 3px 0;
+  padding: 15px 20px;
+  border-radius: 10px;
+  font-size: 25px;
+  background-color: ${({ theme }) => theme.itemBackground};
+  color: ${({ theme }) => theme.text};
+`;
+
+const Input = ({ placeholder, value, onChangeText, onSubmitEditing }) => {
+  const width = Dimensions.get('window').width;
+  return (
+    <StyledInput
+      value={value}
+      onChangeText={onChangeText}
+      onSubmitEditing={onSubmitEditing}
+      width={width}
+      placeholder={placeholder}
+      maxLength={50}
+      // ios 에서 먹는거같은디? 이런 속성은 docs 에서 적용되는 플랫폼을 꼭 체크해야되는듯
+      autoCapitalize={'none'}
+      autoCorrect={false}
+      returnKeyType={'done'}
+      keyboardAppearance={'dark'}
+      //
+    ></StyledInput>
+  );
+};
+
+Input.prototype = {
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  onChangeText: PropTypes.func.isRequired,
+  onSubmitEditing: PropTypes.func.isRequired,
+};
+
+export default Input;
